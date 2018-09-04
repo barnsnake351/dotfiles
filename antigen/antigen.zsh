8a78d69084b4ee0568c8fa0c13f0e410cb2ec82d
Brian Engleman <1727607+barnsnake351@users.noreply.github.com>
2018-09-03 21:02:40 -0700

    additional plugins enabled

--- antigen/antigen.zsh
+++ antigen/antigen.zsh
@@ -7,17 +7,22 @@ if test -f "$antigen_path"
 then
     source "${antigen_path}"
     antigen use oh-my-zsh
+    antigen bundle aws
     antigen bundle git
     antigen bundle pip
     antigen bundle lein
     antigen bundle kubectl
     antigen bundle iterm2
     antigen bundle command-not-found
+    antigen bundle autojump
+    antigen bundle common-aliases
+    antigen bundle git-extras
+    antigen bundle git-flow
 
     antigen bundle zsh-users/zsh-syntax-highlighting
     antigen bundle zsh-users/zsh-completions
 
-    antigen theme robbyrussell
+    antigen theme https://github.com/denysdovhan/spaceship-prompt spaceship
 
     antigen apply
 fi
