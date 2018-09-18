7ef2abd28aa3f260d8c37f7ce4fa160e26101353
Brian Engleman <1727607+barnsnake351@users.noreply.github.com>
2018-09-17 22:01:46 -0700

    more tuning

--- go/path.zsh
+++ go/path.zsh
@@ -1,6 +1,6 @@
 # Configure paths for Go
 
-if (( $+commands[go] )) && [[ -d "${PROJECTS}/go/bin" ]]
+if [[ $(which go) ]] && [[ -d "${PROJECTS}/go/bin" ]]
 then
     export GOPATH=$PROJECTS/go
     export PATH="$GOPATH/bin:$PATH"
