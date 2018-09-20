0e85a6dbe23f2ae0d6257636f7a39fea8b94c5f3
Brian Engleman <1727607+barnsnake351@users.noreply.github.com>
2018-09-20 07:14:39 -0700

    minor updates

--- macos/install.sh
+++ macos/install.sh
@@ -5,3 +5,16 @@
 
 echo "› sudo softwareupdate -i -a"
 sudo softwareupdate -i -a
+
+# Configure dock settings
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock mineffect -string suck
+defaults write comm.apple.dock tilesize -integer 36
+defaults write com.apple.dock largesize -integer 96
+defaults write com.apple.dock autohide-delay 0.5
+defaults write com.apple.dock persistent-apps
+defaults write com.apple.dock persistent-others
+defaults write com.apple.dock static-only -bool true
+
+# Apply dock settings:
+killall Dock
